787c6ffce749ce9c5ff51e761015dc05fdd057e0
Luxoru <luxoru.dev@gmail.com>
2025-03-27 00:44:10 +0000

    Added entry poiny

--- JsonFlow-core/src/main/java/me/luxoru/jsonflow/core/JsonFlow.java
+++ JsonFlow-core/src/main/java/me/luxoru/jsonflow/core/JsonFlow.java
@@ -1,44 +1,72 @@
 package me.luxoru.jsonflow.core;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import lombok.SneakyThrows;
-import me.luxoru.jsonflow.core.entity.JsonEntity;
-import me.luxoru.jsonflow.core.file.JsonFile;
-import me.luxoru.jsonflow.core.file.manager.AbstractJsonFileManager;
+import me.luxoru.jsonflow.api.entity.JsonEntity;
+import me.luxoru.jsonflow.api.manager.JsonEntityManager;
+import me.luxoru.jsonflow.core.entity.RawJsonEntity;
+import me.luxoru.jsonflow.core.manager.AbstractJsonEntityManager;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
-public class JsonFlow {
+public final class JsonFlow {
 
-    private static final ObjectMapper objectMapper = new ObjectMapper()
-            .enable(SerializationFeature.INDENT_OUTPUT);
-    private static final AbstractJsonFileManager manager = new AbstractJsonFileManager();
+    private static JsonEntityManager manager;
+    private static boolean initialized = false;
 
-    public static <T extends JsonEntity<T>> T load(String fileName, Class<T> clazz){
-        return load(new File(fileName), clazz);
+
+    public static void init(){
+        manager = new AbstractJsonEntityManager();
+        initialized = true;
     }
 
-    @SneakyThrows
-    public static <T extends JsonEntity<T>> T load(File file, Class<T> clazz){
+    public static JsonEntity<?> getEntity(String name){
+        return manager.getEntity(name);
+    }
 
-        JsonFile<T> jsonFile = manager.readFile(file, clazz, true);
-        if(jsonFile == null){
-            System.out.println("Couldn't read file: "+file.getName());
+    public static <T extends JsonEntity<T>> JsonEntity<T> getEntity(String name, Class<T> clazz){
+        return manager.getEntity(name, clazz);
+    }
+
+    public static Collection<JsonEntity<?>> getEntities(){
+        return manager.getEntities();
+    }
+
+    public static Set<String> getFileNames(){
+        return manager.getFileNames();
+    }
+
+    public static Map<String, JsonEntity<?>> getFileMap(){
+        return manager.getFileMap();
+    }
+
+    public static RawJsonEntity loadRaw(File jsonFile){
+        return load(jsonFile, RawJsonEntity.class);
+    }
+
+    public static RawJsonEntity loadRaw(File jsonFile, boolean addFileToCache){
+        return load(jsonFile, RawJsonEntity.class, addFileToCache);
+    }
+
+    public static <T extends JsonEntity<T>> T load(File file, Class<T> clazz) {
+        return load(file, clazz, false);
+    }
+
+    public static <T extends JsonEntity<T>> T load(File jsonFile,Class<T> jsonClazz, boolean addFileToCache){
+        if(!initialized){
+            init();
+        }
+        try {
+            return manager.readFile(jsonFile, jsonClazz,addFileToCache);
+        } catch (FileNotFoundException e) {
+            System.out.println("Error occurred loading file");
             return null;
         }
-        return jsonFile.getJsonEntity();
-
-
-
     }
 
-    public static File save(JsonEntity<?> entity){
-        return entity.save();
-    }
 
-    public static File save(JsonEntity<?> entity, File loc){
-        return entity.save(loc);
-    }
+
 
 }
