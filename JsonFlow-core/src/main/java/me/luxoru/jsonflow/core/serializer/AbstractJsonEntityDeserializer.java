87dd5263ad7eb49ae353a9d71c0573fda064bcd1
Luxoru <luxoru.dev@gmail.com>
2025-03-27 00:46:37 +0000

    Working on squash optimisation

--- JsonFlow-core/src/main/java/me/luxoru/jsonflow/core/serializer/AbstractJsonEntityDeserializer.java
+++ JsonFlow-core/src/main/java/me/luxoru/jsonflow/core/serializer/AbstractJsonEntityDeserializer.java
@@ -3,61 +3,45 @@ package me.luxoru.jsonflow.core.serializer;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.RequiredArgsConstructor;
 import me.luxoru.jsonflow.core.entity.AbstractJsonEntity;
-import me.luxoru.jsonflow.core.entity.JsonEntity;
+
 import me.luxoru.jsonflow.core.entity.RawJsonEntity;
-import me.luxoru.jsonflow.core.file.JsonFile;
-import me.luxoru.jsonflow.core.file.manager.AbstractJsonFileManager;
-import me.luxoru.jsonflow.core.file.manager.JsonFileManager;
+import me.luxoru.jsonflow.core.manager.AbstractJsonEntityManager;
+import me.luxoru.jsonflow.api.manager.JsonEntityManager;
 
 import java.io.File;
 import java.io.IOException;
 
+@RequiredArgsConstructor
 public abstract class AbstractJsonEntityDeserializer<T extends AbstractJsonEntity<T>> extends JsonDeserializer<AbstractJsonEntity<T>> {
 
     protected static final ObjectMapper objectMapper = new ObjectMapper();
 
-    private final JsonFileManager fileManager;
+    private final JsonEntityManager fileManager;
 
     public AbstractJsonEntityDeserializer(){
-        this.fileManager = new AbstractJsonFileManager();
-    }
-
-    public AbstractJsonEntityDeserializer(JsonFileManager manager){
-        this.fileManager = manager;
+        this.fileManager = new AbstractJsonEntityManager();
     }
 
     @Override
     public final AbstractJsonEntity<T> deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-
-        System.out.println("ABSTRACT1");
 
         ObjectNode node = p.getCodec().readTree(p);
         String parent = null;
 
         if(node.get("parent") != null){
             parent = node.get("parent").asText();
-            System.out.println("Fetching parent: "+parent);;
-
             if(!parent.endsWith(".json")){
                 parent += ".json";
             }
         }
 
-
-
-
-        //TODO: Find parent json and deserialise
         RawJsonEntity jsonEntity = null;
         if(parent != null){
-            System.out.println("-----NIG-----");
-            JsonFile<RawJsonEntity> abstractJsonEntityJsonFile = fileManager.readFile(new File(parent), RawJsonEntity.class);
-            jsonEntity = abstractJsonEntityJsonFile.getJsonEntity();
-
-            System.out.println("-----GER-----");
+            jsonEntity = fileManager.readFile(new File(parent), RawJsonEntity.class);
         }
 
         //Squash tree
@@ -73,13 +57,7 @@ public abstract class AbstractJsonEntityDeserializer<T extends AbstractJsonEntit
 
         }
 
-
-
-
         T entityData = getEntityData(node);
-
-
-        System.out.println("BOOM1: "+node);
 
         if(jsonEntity != null){
             entityData.setParent(jsonEntity);
