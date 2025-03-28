0845858178213a69f683aea1997c2aafdc17ac45
Luxoru <luxoru.dev@gmail.com>
2025-03-28 23:53:56 +0000

    remove code dup

---
+++ JsonFlow-core/src/main/java/me/luxoru/jsonflow/core/serializer/EntityDeserializer.java
@@ -0,0 +1,55 @@
+package me.luxoru.jsonflow.core.serializer;
+
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import me.luxoru.jsonflow.api.entity.JsonEntity;
+import me.luxoru.jsonflow.core.JsonFlow;
+import me.luxoru.jsonflow.core.entity.RawJsonEntity;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+
+public abstract class EntityDeserializer<T> extends JsonDeserializer<T> {
+
+    protected JsonEntity mergeWithParentJson(ObjectNode node){
+        String parent = null;
+
+        if(node.get("parent") != null){
+            parent = node.get("parent").asText();
+
+            if(!parent.endsWith(".json")){
+                parent += ".json";
+            }
+        }
+        RawJsonEntity jsonEntity = null;
+        if(parent != null) {
+            URL url = AbstractJsonEntityDeserializer.class.getClassLoader().getResource(parent);
+
+            if (url != null) {
+
+                try {
+                    jsonEntity = JsonFlow.load(Paths.get(url.toURI()).toFile(), RawJsonEntity.class);
+                } catch (URISyntaxException _) {
+                }
+            }
+
+
+        }
+
+        //Squash tree
+        if(jsonEntity != null){
+            ObjectNode jsonObject = jsonEntity.toJsonObject();
+            jsonObject.fields().forEachRemaining(entry ->{
+                if(!node.has(entry.getKey())){
+                    node.set(entry.getKey(), entry.getValue());
+                }
+            });
+            node.remove("parent");
+            node.remove("type");
+
+        }
+        return jsonEntity;
+    }
+
+}
