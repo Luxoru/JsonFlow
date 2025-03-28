795c7d8ae14b63ef961b1e3e1462a5e3a714903e
Luxoru <luxoru.dev@gmail.com>
2025-03-28 14:50:57 +0000

    Added deserializer option for fields

--- JsonFlow-API/src/main/java/me/luxoru/jsonflow/api/annotation/FlowField.java
+++ JsonFlow-API/src/main/java/me/luxoru/jsonflow/api/annotation/FlowField.java
@@ -1,5 +1,7 @@
 package me.luxoru.jsonflow.api.annotation;
 
+import me.luxoru.jsonflow.api.serialize.JsonFlowDeserializer;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -10,5 +12,7 @@ import java.lang.annotation.Target;
 public @interface FlowField {
 
     String fieldName();
+
+    Class<? extends JsonFlowDeserializer> serializer()  default JsonFlowDeserializer.class;
 
 }
