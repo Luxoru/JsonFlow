04d5ba7506e252e095cb0a7683cbe3eedcf959ad
Luxoru <luxoru.dev@gmail.com>
2025-03-28 23:46:35 +0000

    Testing examples

--- JsonFlow-Example/src/main/java/me/luxoru/example/EntityExample.java
+++ JsonFlow-Example/src/main/java/me/luxoru/example/EntityExample.java
@@ -1,5 +1,6 @@
 package me.luxoru.example;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import me.luxoru.example.entity.impl.Player;
 import me.luxoru.jsonflow.api.entity.JsonEntity;
 import me.luxoru.jsonflow.core.JsonFlow;
@@ -23,11 +24,12 @@ public class EntityExample {
         File file = Paths.get(resourceUrl.toURI()).toFile();
 
         Player player = JsonFlow.load(file, Player.class);
-        JsonEntity entity = JsonFlow.getEntity(file.getName(), Player.class);
 
-        player.damage(2.3f);
+        System.out.println(player.toJsonObject().toPrettyString());
 
-        player.move(-2.2f, 3.2f);
+        player.move(2,2);
+
+        System.out.println(player.toJsonObject().toPrettyString());
 
     }
 
