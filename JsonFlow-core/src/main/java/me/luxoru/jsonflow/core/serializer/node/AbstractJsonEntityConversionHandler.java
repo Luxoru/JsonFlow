0c43c28d53ede2ce5e80bc15987a1d1a1ac10bec
Luxoru <luxoru.dev@gmail.com>
2025-03-31 11:01:40 +0100

    Merged interfaces

--- JsonFlow-core/src/main/java/me/luxoru/jsonflow/core/serializer/node/AbstractJsonEntityConversionHandler.java
+++ JsonFlow-core/src/main/java/me/luxoru/jsonflow/core/serializer/node/AbstractJsonEntityConversionHandler.java
@@ -1,14 +1,11 @@
 package me.luxoru.jsonflow.core.serializer.node;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Getter;
+import me.luxoru.jsonflow.api.serialize.JsonFlowConversionHandler;
 import me.luxoru.jsonflow.api.serialize.JsonFlowDeserializer;
 import me.luxoru.jsonflow.api.serialize.JsonFlowSerializer;
 
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-
-public abstract class AbstractJsonEntity<T> implements JsonFlowDeserializer<T>, JsonFlowSerializer<T> {
+public abstract class AbstractJsonEntityConversionHandler<T> implements JsonFlowConversionHandler<T> {
 
     protected static final ObjectMapper objectMapper = new ObjectMapper();
 
