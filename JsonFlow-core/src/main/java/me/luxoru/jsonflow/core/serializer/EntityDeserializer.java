e008399453d65d81a771bfe3767d9e13e5b074df
Luxoru <luxoru.dev@gmail.com>
2025-03-31 11:01:07 +0100

    Class type set through constructor

--- JsonFlow-core/src/main/java/me/luxoru/jsonflow/core/serializer/EntityDeserializer.java
+++ JsonFlow-core/src/main/java/me/luxoru/jsonflow/core/serializer/EntityDeserializer.java
@@ -2,15 +2,31 @@ package me.luxoru.jsonflow.core.serializer;
 
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.Getter;
 import me.luxoru.jsonflow.api.entity.JsonEntity;
 import me.luxoru.jsonflow.core.JsonFlow;
 import me.luxoru.jsonflow.core.entity.RawJsonEntity;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
 
 public abstract class EntityDeserializer<T> extends JsonDeserializer<T> {
+
+    @Getter
+    private final Class<T> entityClass;
+
+    protected EntityDeserializer() {
+        Type type = getClass().getGenericSuperclass();
+        if (type instanceof ParameterizedType) {
+            Type actualType = ((ParameterizedType) type).getActualTypeArguments()[0];
+            this.entityClass = (Class<T>) actualType; // Cast to Class<T>
+        } else {
+            throw new RuntimeException("Could not determine generic type.");
+        }
+    }
 
     protected JsonEntity mergeWithParentJson(ObjectNode node){
         String parent = null;
