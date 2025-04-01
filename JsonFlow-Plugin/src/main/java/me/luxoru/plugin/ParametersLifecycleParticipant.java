31d541bb86fc1af1604b0138cc6c26a0d1fcc668
Luxoru <luxoru.dev@gmail.com>
2025-04-01 11:14:08 +0100

    working on flowIgnore

--- JsonFlow-Plugin/src/main/java/me/luxoru/plugin/ParametersLifecycleParticipant.java
+++ JsonFlow-Plugin/src/main/java/me/luxoru/plugin/ParametersLifecycleParticipant.java
@@ -1,40 +1,53 @@
 package me.luxoru.plugin;
 
 import org.apache.maven.AbstractMavenLifecycleParticipant;
-import org.apache.maven.MavenExecutionException;
 import org.apache.maven.execution.MavenSession;
+import org.apache.maven.model.Plugin;
+import org.apache.maven.model.Build;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
 
-import java.util.List;
-
-@Component(role = AbstractMavenLifecycleParticipant.class)
+@Component(role = AbstractMavenLifecycleParticipant.class, hint = "configure-parameters")
 public class ParametersLifecycleParticipant extends AbstractMavenLifecycleParticipant {
 
     @Override
-    public void afterProjectsRead(MavenSession session) throws MavenExecutionException {
-        List<MavenProject> projects = session.getProjects();
-
-        for (MavenProject project : projects) {
-            // Check if our plugin is a dependency
-            if (hasDependency(project, "me.luxoru", "jsonflow-core")) {
-                if (!project.getProperties().containsKey("maven.compiler.parameters")) {
-                    project.getProperties().setProperty("maven.compiler.parameters", "true");
-                }
-
-                Object compilerArgs = project.getProperties().get("maven.compiler.args");
-                if (compilerArgs == null) {
-                    project.getProperties().setProperty("maven.compiler.args", "-parameters");
-                } else if (!compilerArgs.toString().contains("-parameters")) {
-                    project.getProperties().setProperty("maven.compiler.args",
-                            compilerArgs.toString() + " -parameters");
-                }
-            }
-        }
+    public void afterProjectsRead(MavenSession session) {
+        session.getAllProjects().forEach(project -> {
+            ensureParametersFlag(project);
+        });
     }
 
-    private boolean hasDependency(MavenProject project, String groupId, String artifactId) {
-        return project.getDependencies().stream()
-                .anyMatch(d -> groupId.equals(d.getGroupId()) && artifactId.equals(d.getArtifactId()));
+    private void ensureParametersFlag(MavenProject project) {
+        Build build = project.getBuild();
+        if (build == null) {
+            build = new Build();
+            project.setBuild(build);
+        }
+
+        Plugin compilerPlugin = build.getPluginsAsMap().get("org.apache.maven.plugins:maven-compiler-plugin");
+        if (compilerPlugin == null) {
+            compilerPlugin = new Plugin();
+            compilerPlugin.setGroupId("org.apache.maven.plugins");
+            compilerPlugin.setArtifactId("maven-compiler-plugin");
+            compilerPlugin.setVersion("3.8.1");
+            build.addPlugin(compilerPlugin);
+        }
+
+        Xpp3Dom configuration = (Xpp3Dom) compilerPlugin.getConfiguration();
+        if (configuration == null) {
+            configuration = new Xpp3Dom("configuration");
+        }
+
+        Xpp3Dom parametersNode = configuration.getChild("parameters");
+        if (parametersNode == null) {
+            parametersNode = new Xpp3Dom("parameters");
+            parametersNode.setValue("true");
+            configuration.addChild(parametersNode);
+        } else {
+            parametersNode.setValue("true");
+        }
+
+        compilerPlugin.setConfiguration(configuration);
     }
 }
--- JsonFlow-Plugin/src/main/java/me/luxoru/plugin/ParametersMojo.java
+++ JsonFlow-Plugin/src/main/java/me/luxoru/plugin/ParametersMojo.java
@@ -1,5 +1,7 @@
 package me.luxoru.plugin;
 
+import org.apache.maven.model.Build;
+import org.apache.maven.model.Plugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -8,9 +10,9 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.utils.xml.Xpp3Dom;
 
-@Mojo(name = "configure-parameters", defaultPhase = LifecyclePhase.VALIDATE,
-        requiresDependencyResolution = ResolutionScope.COMPILE)
+@Mojo(name = "configure-parameters", defaultPhase = LifecyclePhase.VALIDATE)
 public class ParametersMojo extends AbstractMojo {
 
     @Parameter(defaultValue = "${project}", readonly = true, required = true)
@@ -18,20 +20,44 @@ public class ParametersMojo extends AbstractMojo {
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("Configuring compiler to preserve parameter names");
+        getLog().info("Ensuring -parameters flag is set in Maven Compiler Plugin");
 
-        // Get or create compiler configuration
-        Object compilerConfig = project.getProperties().get("maven.compiler.args");
-
-        // Add -parameters flag if it doesn't exist
-        if (compilerConfig == null) {
-            project.getProperties().setProperty("maven.compiler.args", "-parameters");
-        } else if (!compilerConfig.toString().contains("-parameters")) {
-            project.getProperties().setProperty("maven.compiler.args",
-                    compilerConfig.toString() + " -parameters");
+        // Ensure the project has a build section
+        Build build = project.getBuild();
+        if (build == null) {
+            build = new Build();
+            project.setBuild(build);
         }
 
-        // Set these for backward compatibility
-        project.getProperties().setProperty("maven.compiler.parameters", "true");
+        // Get existing compiler plugin or create a new one
+        Plugin compilerPlugin = build.getPluginsAsMap().get("org.apache.maven.plugins:maven-compiler-plugin");
+        if (compilerPlugin == null) {
+            compilerPlugin = new Plugin();
+            compilerPlugin.setGroupId("org.apache.maven.plugins");
+            compilerPlugin.setArtifactId("maven-compiler-plugin");
+            compilerPlugin.setVersion("3.8.1"); // Adjust as needed
+            build.addPlugin(compilerPlugin);
+        }
+
+        // Ensure <configuration> exists
+        Xpp3Dom configuration = (Xpp3Dom) compilerPlugin.getConfiguration();
+        if (configuration == null) {
+            configuration = new Xpp3Dom("configuration");
+        }
+
+        // Ensure <parameters>true</parameters> is set
+        Xpp3Dom parametersNode = configuration.getChild("parameters");
+        if (parametersNode == null) {
+            parametersNode = new Xpp3Dom("parameters");
+            parametersNode.setValue("true");
+            configuration.addChild(parametersNode);
+        } else {
+            parametersNode.setValue("true");
+        }
+
+        // Apply the modified configuration
+        compilerPlugin.setConfiguration(configuration);
+
+        getLog().info("Successfully enforced -parameters in Maven Compiler Plugin.");
     }
 }
