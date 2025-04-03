653ca51bb8a902178adbfda63831db5a2b78cabb
Luxoru <luxoru.dev@gmail.com>
2025-04-03 22:41:11 +0100

    Added serializer for class

--- JsonFlow-API/src/main/java/me/luxoru/jsonflow/api/annotation/FlowSerializable.java
+++ JsonFlow-API/src/main/java/me/luxoru/jsonflow/api/annotation/FlowSerializable.java
@@ -1,5 +1,7 @@
 package me.luxoru.jsonflow.api.annotation;
 
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,4 +10,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface FlowSerializable {
+
+    Class<? extends JsonDeserializer> using()
+            default JsonDeserializer.None.class;
+
 }
