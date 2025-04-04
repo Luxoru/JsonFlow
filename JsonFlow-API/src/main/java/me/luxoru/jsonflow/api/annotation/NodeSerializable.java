3226eb528436cc0345d779df304608f13219f55f
Luxoru <luxoru.dev@gmail.com>
2025-04-05 00:55:17 +0100

    Working on serializing nodes

--- JsonFlow-API/src/main/java/me/luxoru/jsonflow/api/annotation/NodeSerializable.java
+++ JsonFlow-API/src/main/java/me/luxoru/jsonflow/api/annotation/NodeSerializable.java
@@ -2,6 +2,13 @@ package me.luxoru.jsonflow.api.annotation;
 
 import me.luxoru.jsonflow.api.serialize.JsonNodeConversionHandler;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = ElementType.TYPE)
 public @interface NodeSerializable {
 
     Class<? extends JsonNodeConversionHandler> serializer();
