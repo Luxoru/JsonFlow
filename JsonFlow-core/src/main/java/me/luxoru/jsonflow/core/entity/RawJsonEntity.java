4e1aff8347be48c88a81f58d144031aca24d33d1
Luxoru <luxoru.dev@gmail.com>
2025-04-04 18:58:40 +0100

    Removed persistance

--- JsonFlow-core/src/main/java/me/luxoru/jsonflow/core/entity/RawJsonEntity.java
+++ JsonFlow-core/src/main/java/me/luxoru/jsonflow/core/entity/RawJsonEntity.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import me.luxoru.jsonflow.api.entity.PersistableEntity;
 import me.luxoru.jsonflow.core.serializer.RawJsonEntityDeserializer;
 import java.io.File;
 import java.util.LinkedHashMap;
@@ -16,7 +15,7 @@ import java.util.Map;
  */
 @JsonDeserialize(using = RawJsonEntityDeserializer.class)
 @RequiredArgsConstructor
-public final class RawJsonEntity extends AbstractJsonEntity implements PersistableEntity {
+public final class RawJsonEntity extends AbstractJsonEntity{
 
     private final LinkedHashMap<String, JsonNode> pairs;
 
@@ -31,16 +30,5 @@ public final class RawJsonEntity extends AbstractJsonEntity implements Persistab
             node.set(entry.getKey(), entry.getValue());
         }
         return node;
-    }
-
-    @SneakyThrows
-    @Override
-    public File save(File fileToSave) {
-        return null;
-    }
-
-    @Override
-    public File save() {
-        return null;
     }
 }
