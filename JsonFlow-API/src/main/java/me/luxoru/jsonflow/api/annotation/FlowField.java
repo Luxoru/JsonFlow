a5f599e175b3ba98d437ef9586efb96d19e180ee
Luxoru <luxoru.dev@gmail.com>
2025-04-04 18:56:55 +0100

    Made value optional

--- JsonFlow-API/src/main/java/me/luxoru/jsonflow/api/annotation/FlowField.java
+++ JsonFlow-API/src/main/java/me/luxoru/jsonflow/api/annotation/FlowField.java
@@ -1,6 +1,6 @@
 package me.luxoru.jsonflow.api.annotation;
 
-import me.luxoru.jsonflow.api.serialize.JsonFlowConversionHandler;
+import me.luxoru.jsonflow.api.serialize.JsonNodeConversionHandler;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -10,8 +10,8 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 public @interface FlowField {
 
-    String value();
+    String value() default "";
 
-    Class<? extends JsonFlowConversionHandler> serializer()  default JsonFlowConversionHandler.class;
+    Class<? extends JsonNodeConversionHandler> serializer()  default JsonNodeConversionHandler.class;
 
 }
