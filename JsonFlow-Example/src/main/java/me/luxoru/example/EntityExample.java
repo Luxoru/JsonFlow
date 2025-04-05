dca027210cfcf5f01eeaaeee2fd03f37bd59ecd2
Luxoru <luxoru.dev@gmail.com>
2025-04-06 00:02:05 +0100

    Example update

--- JsonFlow-Example/src/main/java/me/luxoru/example/EntityExample.java
+++ JsonFlow-Example/src/main/java/me/luxoru/example/EntityExample.java
@@ -7,27 +7,17 @@ public class EntityExample {
 
     public static void main(String[] args) {
 
-        System.out.println("Loading player");
+        Player player = JsonFlow.load("player", Player.class);
 
-        Player player  = null;
-        double totalTimeTaken = 0;
-        final int interations = 10_000;
+        String json = player.toJsonObject().toPrettyString(); //Takes fields from POJO and converts to json
 
-        for(int i =0;i<interations;i++){
-            long start = System.currentTimeMillis();
-            player = JsonFlow.load("player", Player.class, false);
-            totalTimeTaken += System.currentTimeMillis() - start;
-        }
-
-        System.out.printf("Average Loaded player in %s ms%n", (totalTimeTaken/interations));
-
-
-        System.out.println(player.toJsonObject().toPrettyString());
+        System.out.println(json);
 
         player.move(2,2);
-        player.damage(7);
 
-        System.out.println(player.toJsonObject().toPrettyString());
+        System.out.println(player.getPosition());
+
+
 
     }
 
